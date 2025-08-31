4c7f06b875279a8a411a8061600a4e4d63eec543
Стецурин Леонид Дмитриевич <Bozhenkas@yandex.ru>
2025-08-31 16:10:35 +0300

    add arrow in back buttons

--- src/bot/keyboards/purchase.py
+++ src/bot/keyboards/purchase.py
@@ -16,6 +16,7 @@ async def kb_choosing_price() -> types.InlineKeyboardMarkup:
         [types.InlineKeyboardButton(text='с репостом', callback_data='repost:true'),
          types.InlineKeyboardButton(text='без репоста', callback_data='repost:false')],
         [types.InlineKeyboardButton(text='сделать репост', url='https://instagram.com/belekkerx')],
+        [types.InlineKeyboardButton(text='⤝ назад', callback_data='back')]
     ]
     return types.InlineKeyboardMarkup(inline_keyboard=k)
 
@@ -30,15 +31,13 @@ async def kb_quantity() -> types.InlineKeyboardMarkup:
 async def kb_confirm_paid() -> types.InlineKeyboardMarkup:
     k = [
         [types.InlineKeyboardButton(text='я перевел', callback_data='paid:confirm')],
-        [types.InlineKeyboardButton(text='назад', callback_data='back')]
+        [types.InlineKeyboardButton(text='⤝ назад', callback_data='back')]
     ]
     return types.InlineKeyboardMarkup(inline_keyboard=k)
 
 
 async def kb_promo_code() -> types.InlineKeyboardMarkup:
     k = [
-        [types.InlineKeyboardButton(text='назад', callback_data='back')],
+        [types.InlineKeyboardButton(text='⤝ назад', callback_data='back')],
     ]
     return types.InlineKeyboardMarkup(inline_keyboard=k)
-
-
